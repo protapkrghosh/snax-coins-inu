841322dd46bd1d3039200e1188db0d608cb44e03
Protap Ghosh <protapg518@gmail.com>
2024-05-20 05:10:59 +0600

    sideMenu completed

--- src/components/Sections/SideMenu/SideMenu.jsx
+++ src/components/Sections/SideMenu/SideMenu.jsx
@@ -8,9 +8,9 @@ const SideMenu = () => {
   return (
     <div>
       <div className="hidden md:block ">
-        <img src={buttonBg} alt="" className='mb-[-500px] md:w-[65px] lg:w-[74px] xl:w-[100px] 2xl:w-[109px] md:h-[281px] lg:h-[288px] xl:h-[360px] 2xl:h-[455px] absolute top-[460px] md:top-[297px] lg:top-[319px] xl:top-[389px] 2xl:top-[368px] right-24 md:right-[46px] lg:right-[65px] xl:right-[55px] 2xl:right-[99.6px]'/>
+        <img src={buttonBg} alt="Image" className='mb-[-500px] md:w-[65px] lg:w-[75px] xl:w-[100px] 2xl:w-[109px] md:h-[279px] lg:h-[288px] xl:h-[360px] 2xl:h-[455px] absolute md:top-[213px] lg:top-[239px] xl:top-[299px] 2xl:top-[368px] right-24 md:right-[38px] lg:right-[56px] xl:right-[55px] 2xl:right-[99.6px]'/>
 
-        <div className="absolute top-[460px] md:top-[300px] lg:top-[320px] xl:top-[390px] 2xl:top-[370px] right-24 md:right-5 lg:right-12 xl:right-16 2xl:right-[117px]">
+        <div className="absolute md:top-[215px] lg:top-[240px] xl:top-[300px] 2xl:top-[370px] right-24 md:right-3 lg:right-10 xl:right-16 2xl:right-[117px]">
           <div className='w-[90px]'>
             <div className='cusBorder w-fit flex flex-col justify-center  rounded-full'>
               <div className="p-[10px] lg:p-[15px] xl:p-[20px] bg-[558562] rounded-full space-y-[30px] 2xl:space-y-[50px]">
